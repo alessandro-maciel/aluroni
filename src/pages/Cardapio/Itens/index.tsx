6b07c14197d2969689a4b19bdb3ff4c75311f621
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-05-03 11:57:35 -0300

    feat: funcionalidade de busca e filtro

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from 'react';
 import Item from './Item';
 import items from './itens.json';
 import styles from './Itens.module.scss';
 
-export default function Itens(){
+interface Props {
+    busca: string;
+    filtro: number | null;
+    ordenador: string;
+}
+
+export default function Itens(props: Props){
+    const [lista, setLista] = useState(items);
+    const { busca, filtro } = props;
+
+    function testaBusca(title: string){
+        const regex = new RegExp(busca, 'i');
+
+        return regex.test(title);
+    }
+
+    function testaFiltro(id: number | null){
+        if (filtro !== null) {
+            return filtro === id;
+        }
+
+       return true;
+    }
+
+    useEffect(() => {
+        const novaLista = items.filter(
+            item => testaBusca(item.title) && testaFiltro(item.category.id)
+        );
+
+        setLista(novaLista);
+    },[busca, filtro]);
+
     return (
         <div className={styles.itens}>
-            {items.map(item => (
+            {lista.map(item => (
                 <Item key={item.id} {...item}/>
             ))}
         </div>
