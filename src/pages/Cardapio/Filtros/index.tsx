718ecfe73fdac1ba9b247737997761e849b847da
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-05-03 21:13:28 -0300

    fix: eslint

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -6,36 +6,36 @@ import classNames from 'classnames';
 type Opcao = typeof filtros[0];
 
 interface Props {
-    filtro: number | null, 
-    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+  filtro: number | null,
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>
 }
 
-export default function Filtros({filtro, setFiltro}: Props){
+export default function Filtros({ filtro, setFiltro }: Props) {
 
-    function selecionarFiltros(opcao: Opcao){
-        if (filtro === opcao.id) {
-            setFiltro(null);
+  function selecionarFiltros(opcao: Opcao) {
+    if (filtro === opcao.id) {
+      setFiltro(null);
 
-            return;
-        }
-
-        setFiltro(opcao.id);
+      return;
     }
 
-    return (
-        <div className={styles.filtros}>
-            {filtros.map((opcao) => (
-                <button
-                    className={classNames({
-                        [styles.filtros__filtro]: true,
-                        [styles['filtros__filtro--ativo']]: filtro === opcao.id
-                    })} 
-                    key={opcao.id} 
-                    onClick={() => selecionarFiltros(opcao)}
-                >
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
-    );
+    setFiltro(opcao.id);
+  }
+
+  return (
+    <div className={styles.filtros}>
+      {filtros.map((opcao) => (
+        <button
+          className={classNames({
+            [styles.filtros__filtro]: true,
+            [styles['filtros__filtro--ativo']]: filtro === opcao.id
+          })}
+          key={opcao.id}
+          onClick={() => selecionarFiltros(opcao)}
+        >
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  );
 }
--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import Item from './Item';
 import items from './itens.json';
 import styles from './Itens.module.scss';
-import Ordenador from '../Ordenador';
 
 interface Props {
     busca: string;
@@ -11,49 +10,49 @@ interface Props {
 }
 
 export default function Itens(props: Props){
-    const [lista, setLista] = useState(items);
-    const { busca, filtro, ordenador } = props;
+  const [lista, setLista] = useState(items);
+  const { busca, filtro, ordenador } = props;
 
-    function testaBusca(title: string){
-        const regex = new RegExp(busca, 'i');
+  function testaBusca(title: string){
+    const regex = new RegExp(busca, 'i');
 
-        return regex.test(title);
+    return regex.test(title);
+  }
+
+  function testaFiltro(id: number | null){
+    if (filtro !== null) {
+      return filtro === id;
     }
 
-    function testaFiltro(id: number | null){
-        if (filtro !== null) {
-            return filtro === id;
-        }
+    return true;
+  }
 
-       return true;
+  function ordenar(novaLista: typeof items){
+    switch (ordenador) {
+    case 'porcao':
+      return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+    case 'qtd_pessoas':
+      return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
+    case 'preco':
+      return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
+    default:
+      return novaLista;
     }
+  }
 
-    function ordenar(novaLista: typeof items){
-        switch (ordenador) {
-            case 'porcao':
-                return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
-            case 'qtd_pessoas':
-                return novaLista.sort((a, b) => a.serving > b.serving ? 1 : -1);
-            case 'preco':
-                return novaLista.sort((a, b) => a.price > b.price ? 1 : -1);
-            default:
-                return novaLista;
-        }
-    }
-
-    useEffect(() => {
-        const novaLista = items.filter(
-            item => testaBusca(item.title) && testaFiltro(item.category.id)
-        );
-
-        setLista(ordenar(novaLista));
-    },[busca, filtro, ordenador]);
-
-    return (
-        <div className={styles.itens}>
-            {lista.map(item => (
-                <Item key={item.id} {...item}/>
-            ))}
-        </div>
+  useEffect(() => {
+    const novaLista = items.filter(
+      item => testaBusca(item.title) && testaFiltro(item.category.id)
     );
+
+    setLista(ordenar(novaLista));
+  },[busca, filtro, ordenador]);
+
+  return (
+    <div className={styles.itens}>
+      {lista.map(item => (
+        <Item key={item.id} {...item}/>
+      ))}
+    </div>
+  );
 }
